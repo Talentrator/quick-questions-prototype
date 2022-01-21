97ac250ea2bebf30d4b9ce0f156bdf6582429986
james-muriithi <muriithijames556@gmail.com>
2022-01-21 11:01:42 +0300

    added touch event listeners

--- src/components/questions/QuestionCard.vue
+++ src/components/questions/QuestionCard.vue
@@ -1,55 +1,91 @@
 <template>
-  <div class="question-card h-100 p-3">
-      <b-row class="justify-content-center align-items-center h-100">
-          <div class="text-center px-3">
-              <p class="question fs-5">
-                  {{question.text}}
-              </p>
-            <b-row class="px-3 mt-4">
-                <b-col col="6">
-                    <span class="">
-                        {{formatTime(question.time)}}
-                    </span>
-                </b-col>
-                <b-col col="6" class="text-center">
-                    <span class="">
-                        {{question.score}} points
-                    </span>
-                </b-col>
-            </b-row>
-          </div>
-      </b-row>
+  <div
+    class="question-card h-100 p-3"
+    @touchstart="handleTouchStart"
+  >
+    <b-row class="justify-content-center align-items-center h-100">
+      <div class="text-center px-3">
+        <p class="question fs-5">
+          {{ question.text }}
+        </p>
+        <b-row class="px-3 mt-5">
+          <b-col cols="6" class="text-md-center text-start">
+            <span>
+              {{ formatTime(question.time) }}
+            </span>
+          </b-col>
+          <b-col cols="6" class="text-md-center text-end">
+            <span class=""> {{ question.score }} points </span>
+          </b-col>
+        </b-row>
+      </div>
+    </b-row>
   </div>
 </template>
 
 <script>
 export default {
   name: "QuestionCard",
+  props: {
+    question: {
+      type: Object,
+      required: true,
+    },
+  },
   data() {
     return {
-      question: {
-        text: "Which of the following is not a core data type in Python programming?",
-        language: "python",
-        options: [
-          { A: "Lists" },
-          { B: "Tuples" },
-          { C: "Dictionary" },
-          { D: "Class" },
-        ],
-        answer: "D",
-        time: 20, //in seconds
-        score: 5,
-      },
+      xDown: null,
+      yDown: null,
     };
   },
   methods: {
-      formatTime(seconds){
-          const minutes = parseInt(seconds / 60);
-          seconds = seconds % 60;
-          const minutesFormatted = minutes ? `${minutes} min ` : '';
-          const secondssFormatted = seconds ? `${seconds} sec` : '';
-          return `${minutesFormatted}${secondssFormatted}`;
+    formatTime(seconds) {
+      const minutes = parseInt(seconds / 60);
+      seconds = seconds % 60;
+      const minutesFormatted = minutes ? `${minutes} min ` : "";
+      const secondssFormatted = seconds ? `${seconds} sec` : "";
+      return `${minutesFormatted}${secondssFormatted}`;
+    },
+    handleTouchStart(evt) {
+      const firstTouch = evt.touches[0];
+      this.xDown = firstTouch.clientX;
+      this.yDown = firstTouch.clientY;
+
+      addEventListener("touchmove", this.handleTouchMove, false);
+    },
+    handleTouchMove(evt) {
+      if (!this.xDown || !this.yDown) {
+        return;
       }
+
+      var xUp = evt.touches[0].clientX;
+      var yUp = evt.touches[0].clientY;
+
+      var xDiff = this.xDown - xUp;
+      var yDiff = this.yDown - yUp;
+
+      if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        /*most significant*/
+        if (xDiff < 0) {
+          /* right swipe */
+          console.log("right");
+        } else {
+          /* left swipe */
+          console.log("left");
+        }
+      } else {
+        if (yDiff < 0) {
+          /* down swipe */
+          console.log("down");
+        } else {
+          /* up swipe */
+          console.log("up");
+        }
+      }
+      /* reset values */
+      this.xDown = null;
+      this.yDown = null;
+    },
   },
 };
 </script>
--- src/components/questions/QuestionCards.vue
+++ src/components/questions/QuestionCards.vue
@@ -2,7 +2,7 @@
   <div
     class="position-absolute top-0 bottom-0 w-100 flash-screen left-0 bg-white"
   >
-    <question-card />
+    <question-card v-for="question in questions" :question="question" :key="question.id" />
   </div>
 </template>
 
@@ -12,6 +12,26 @@ export default {
   components: { QuestionCard },
   name: "QuestionCards",
   emits: ["close"],
+  data() {
+    return {
+      questions: [
+        {
+          id: 1,
+          text: "Which of the following is not a core data type in Python programming?",
+          language: "python",
+          options: [
+            { A: "Lists" },
+            { B: "Tuples" },
+            { C: "Dictionary" },
+            { D: "Class" },
+          ],
+          answer: "D",
+          time: 20, //in seconds
+          score: 5,
+        },
+      ],
+    };
+  },
   methods: {},
 };
 </script>
